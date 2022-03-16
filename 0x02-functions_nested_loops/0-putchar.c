57c703b18fdd0a179dce351d1fad2880d11fab97
zenawigirmay <zenawigrimay@gmail.com>
2022-03-16 05:43:06 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *main -entry point 
+ *
+ *return: always 0 (success)
+ *
+ */
+int main(void)
+{
+  _putchar(95);
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return(0);
+}
+    
+   
+	
