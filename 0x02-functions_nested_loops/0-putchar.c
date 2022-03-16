841573d3c575ed9a0bedbb09641be56fa1614ebd
zenawigirmay <zenawigrimay@gmail.com>
2022-03-16 05:51:45 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-  _putchar(95);
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-  return(0);
+      _putchar(95);
+      _putchar('p');
+      _putchar('u');
+      _putchar('t');
+      _putchar('c');
+      _putchar('h');
+      _putchar('a');
+      _putchar('r');
+      _putchar('\n');
+       return(0);
 }
     
    
